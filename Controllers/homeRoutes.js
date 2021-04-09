08bdb4b3b53632764896af671bc5818755b2fe92
Breon Sparrow <beyond.committed@gmail.com>
2021-04-09 14:30:18 -0700

    post route for /

--- Controllers/homeRoutes.js
+++ Controllers/homeRoutes.js
@@ -2,6 +2,15 @@ const router = require('express').Router();
 const { Player, Manager } = require('../models');
 const withAuth = require('../utils/auth');
 
-// router.get('/', async (req, res) => {};
-  
+router.get('/', async (req, res) => {
+  try {
+    
+    res.render('login', {  
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
